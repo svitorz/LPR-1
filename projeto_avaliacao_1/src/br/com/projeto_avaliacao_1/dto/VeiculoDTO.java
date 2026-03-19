0fd2bdb2cfed6b46af2d62140861035f0f184b3d
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-03-19 12:23:41 -0300

    feat: create getters and setters

---
+++ projeto_avaliacao_1/src/br/com/projeto_avaliacao_1/dto/VeiculoDTO.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.projeto_avaliacao_1.dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author svitorz
+ */
+public class VeiculoDTO {
+  private int id, ano;
+  private String marca, modelo, cor,
+      placa, chassi, combustivel,
+      proprietario;
+  double quilometragem;
+  float preco;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public String getCor() {
+    return cor;
+  }
+
+  public void setCor(String cor) {
+    this.cor = cor;
+  }
+
+  public String getPlaca() {
+    return placa;
+  }
+
+  public void setPlaca(String placa) {
+    this.placa = placa;
+  }
+
+  public String getChassi() {
+    return chassi;
+  }
+
+  public void setChassi(String chassi) {
+    this.chassi = chassi;
+  }
+
+  public String getCombustivel() {
+    return combustivel;
+  }
+
+  public void setCombustivel(String combustivel) {
+    this.combustivel = combustivel;
+  }
+
+  public String getProprietario() {
+    return proprietario;
+  }
+
+  public void setProprietario(String proprietario) {
+    this.proprietario = proprietario;
+  }
+
+  public double getQuilometragem() {
+    return quilometragem;
+  }
+
+  public void setQuilometragem(double quilometragem) {
+    this.quilometragem = quilometragem;
+  }
+
+  public float getPreco() {
+    return preco;
+  }
+
+  public void setPreco(float preco) {
+    this.preco = preco;
+  }
+
+  public Date getData_cadastro() {
+    return data_cadastro;
+  }
+
+  public void setData_cadastro(Date data_cadastro) {
+    this.data_cadastro = data_cadastro;
+  }
+
+  Date data_cadastro;
+}
