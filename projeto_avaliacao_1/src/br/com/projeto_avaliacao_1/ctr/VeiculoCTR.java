3ab994fe1a59cc493529a258b3f896f1dd07f834
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-03-19 12:51:39 -0300

    feat: vehicle controller

---
+++ projeto_avaliacao_1/src/br/com/projeto_avaliacao_1/ctr/VeiculoCTR.java
@@ -0,0 +1,46 @@
+package br.com.projeto_avaliacao_1.ctr.VeiculoCTR;
+
+import java.sql.*;
+
+import br.com.projeto_1.dao.ConexaoDAO;
+import br.com.projeto_avaliacao_1.dao.VeiculoDAO;
+import br.com.projeto_avaliacao_1.dto.VeiculoDTO;
+
+public class VeiculoCTR {
+  VeiculoDAO dao = new VeiculoDAO();
+
+  public VeiculoCTR() {
+  }
+
+  public String inserirVeiculo(VeiculoDTO dto) {
+    if (dao.inserirVeiculo(dto)) {
+      return "Veículo inserido com sucesso!\n";
+    } else {
+      return "Houve um erro ao inserir veículo.\n";
+    }
+  }
+
+  public ResultSet buscarVeiculo(VeiculoDTO dto, int opcao) {
+    return dao.buscarVeiculo(dto, opcao);
+  }
+
+  public String alterarVeiculo(VeiculoDTO dto) {
+    if (dao.alterarVeiculo(dto)) {
+      return "Veículo atualizado com sucesso!\n";
+    } else {
+      return "Erro ao atualizar veículo.\n";
+    }
+  }
+
+  public String excluirVeiculo(VeiculoDTO dto) {
+    if (dao.excluirVeiculo(dto)) {
+      return "Veículo excluído com sucesso!\n";
+    } else {
+      return "Erro ao excluir veículo.\n";
+    }
+  }
+
+  public void CloseDbHelper() {
+    ConexaoDAO.closeDB();
+  }
+}
