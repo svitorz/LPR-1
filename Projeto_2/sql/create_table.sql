b2fe97ae67586c9738372dd8282d25457db0b259
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-04-16 07:17:51 -0300

    sql: create table script

---
+++ Projeto_2/sql/create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE fornecedor (
+  id_for serial,
+  cnpj_for character varying(15),
+  nome_for character varying(50),
+  tel_for character varying(13),
+  data_cad_for date,
+  CONSTRAINT pk_fornecedor PRIMARY KEY(id_for)
+);
